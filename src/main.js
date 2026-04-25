6b8d56f05b3a6e81b81dd33b40d997b3df8c65bb
SebekGM <s.gasior97@gmail.com>
2026-04-25 04:55:18 +0200

    feat: styled mobile-menu - animation

--- src/main.js
+++ src/main.js
@@ -3,19 +3,12 @@ import './scss/main.scss';
 // MOBILE MENU
 const closeMenu = document.getElementById('close-menu');
 const openMenu = document.getElementById('open-menu');
-const mobileMenuContent = document.getElementById('mobile-menu-content');
 const headerMobile = document.getElementById('header-mobile');
 
 closeMenu.addEventListener('click', () => {
-    mobileMenuContent.classList.add('hidden');
-    openMenu.classList.remove('hidden');
-
     headerMobile.classList.remove('is-visible');
 });
 
 openMenu.addEventListener('click', () => {
-    mobileMenuContent.classList.remove('hidden');
-    openMenu.classList.add('hidden');
-
     headerMobile.classList.add('is-visible');
 });
