37da717f01371a50188c9ae4d9c5c427ead202ee
SebekGM <s.gasior97@gmail.com>
2026-05-02 13:58:40 +0200

    feat: functions of contact form

--- src/main.js
+++ src/main.js
@@ -12,3 +12,29 @@ closeMenu.addEventListener('click', () => {
 openMenu.addEventListener('click', () => {
     headerMobile.classList.add('is-visible');
 });
+
+
+// FORM
+const form = document.getElementById('contact-form');
+const inputName = document.getElementById('input-name');
+const inputLastname = document.getElementById('input-lastname');
+const inputEmail = document.getElementById('input-email');
+const inputPhone = document.getElementById('input-phone');
+const inputMessage = document.getElementById('input-message');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const nameValue = inputName.value;
+    const lastnameValue = inputLastname.value;
+    const emailValue = inputEmail.value;
+    const phoneValue = inputPhone.value;
+    const messageValue = inputMessage.value;
+
+    console.log(`Nowa wiadomość od: ${nameValue} ${lastnameValue}`);
+    console.log(`Adres email: ${emailValue}`);
+    console.log(`Numer telefonu: ${phoneValue}`);
+    console.log(`----------------------------------------------`);
+    console.log(`Treść wiadomości:`);
+    console.log(messageValue);
+})
